4b60fc233c0829ffce5da6d0d7a63f3f883bcf06
unknown <dhruv.r.more14@gmail.com>
2023-02-16 13:08:16 +0530

    small change

---
+++ site/script.js
@@ -0,0 +1,26 @@
+function light()
+{
+    document.getElementById('page').style.backgroundColor='#ffffff';
+    document.getElementById('content').style.color='#3C3C3C';
+    document.getElementById('mainpageheader').style.color='black';
+    document.getElementById('title').style.color='#3c3c3c';
+    document.getElementById('dark').style.borderColor='black';
+    document.getElementById('light').style.borderColor='black';
+    document.getElementById('dark').style.backgroundColor='white';
+    document.getElementById('light').style.backgroundColor='white';
+    document.getElementById('dark').style.color='black';
+    document.getElementById('light').style.color='black';
+}
+function dark()
+{
+    document.getElementById('page').style.backgroundColor='#000000';
+    document.getElementById('content').style.color='#f7f7f7';
+    document.getElementById('mainpageheader').style.color='#f7f7f7';
+    document.getElementById('title').style.color='white';
+    document.getElementById('dark').style.borderColor='white';
+    document.getElementById('light').style.borderColor='white';
+    document.getElementById('dark').style.color='white';
+    document.getElementById('light').style.color='white';
+    document.getElementById('dark').style.backgroundColor='black';
+    document.getElementById('light').style.backgroundColor='black';
+}
